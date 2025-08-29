cd8558f376423f7ed080c492b288c9455aeb206a
raffmont <raffaele.montella@uniparthenope.it>
2025-08-29 18:13:56 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,91 @@
+/*
+ */
+
+export default function (app: any) {
+  const error = app.error
+  const debug = app.debug
+  let putsRegistred: any = {}
+
+  function handler(context: string, path: string, value: number, callback: any) {
+      const timestamp = new Date().toISOString();
+
+    const delta = {
+      context: context,
+      updates: [
+      {
+        source: {label: 'kip-commander'},
+        timestamp: timestamp,
+        values: [{ path: path, value: value}]
+      }
+      ]
+    };
+
+    app.debug('Sending delta:', JSON.stringify(delta));
+    app.handleMessage(context, delta);
+    return { state: 'COMPLETED', statusCode: 200 };
+  }
+
+  const plugin: Plugin = {
+
+    start: function (properties: any) {
+      const logging = {
+        info: (msg: any) => {
+        app.debug(msg)
+        },
+        error: (msg: any) => {
+          app.error(msg)
+        }
+      }
+
+      app.registerPutHandler('vessels.self', 'plugins.kip.mastdisplay.activeDashboard', handler, 'kip-commander.XX');
+      app.registerPutHandler('vessels.self', 'plugins.kip.mastdisplay.maxDashboard', handler, 'kip-commander.XX');
+      app.registerPutHandler('vessels.self', 'plugins.kip.mastdisplay.dashboards', handler, 'kip-commander.XX');
+    },
+
+    stop: function () {
+      putsRegistred = {}
+    },
+
+    id: 'kip-commander',
+    name: 'KIP Commander',
+    description: 'Signal K Plugin For Changing KIP Dashboards remotely',
+
+    schema: () => {
+      const schema: any = {
+        type: 'object',
+        required: ['authInfo'],
+        properties: {
+          authInfo: {
+            type: 'string',
+            lines: 10,
+            title: 'Authentication Info',
+            description: 'See plugin README'
+          }
+        }
+      }
+
+      return schema
+    },
+
+    uiSchema: () => {
+      const uiSchema: any = {
+        authInfo: {
+          'ui:widget': 'textarea'
+        }
+      }
+      return uiSchema
+    }
+  }
+
+  return plugin
+}
+
+interface Plugin {
+  start: (app: any) => void
+  stop: () => void
+  id: string
+  name: string
+  description: string
+  schema: any
+  uiSchema: any
+}
